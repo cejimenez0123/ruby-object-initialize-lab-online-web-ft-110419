c0ac46927f3f47673c78fc3faad5590c349069b8
Christian Jimenez <christianjimenez0123@gmail.com>
2019-11-15 19:00:08 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,15 +3,6 @@ class Person
   def initialize(name)
       @name = name
     end
-def breed=(breed = "Mutt")
-  @breed = breed
-end
 
-    def name
-      @name
-    end
-    def breed
-      @breed
-    end
 
 end
